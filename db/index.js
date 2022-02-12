2012189c789ad0b50b5f0f1a3cf57f78ee5da9b9
Pedro Ortega Escande <ortegapedro88@gmail.com>
2022-02-12 14:03:56 +0100

    first commit

--- db/index.js
+++ db/index.js
@@ -1,14 +1,15 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+require('dotenv/config')
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
+
 
 mongoose
-  .connect(MONGO_URI, {
+ .connect(`mongodb+srv://${process.env.MG_USER}:${process.env.MG_PWD}@cluster0.sexab.mongodb.net/CinemaLab?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
